7dc0882e871cb9d641716b4b9d2a38fa2c4549c9
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-05 20:41:47 +0430

    borrowing field

--- database/migrations/2026_02_05_161111_create_borrowings_table.php
+++ database/migrations/2026_02_05_161111_create_borrowings_table.php
@@ -11,14 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('members', function (Blueprint $table) {
+        Schema::create('borrowings', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->string("email")->unique();
-            $table->string("adress");
-            $table->enum("status",['active','inactive'])->default('active');
-            $table->date("membership_date")->default(now());
-            $table->string("wawhatsSpp_number")->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('members');
+        Schema::dropIfExists('borrowings');
     }
 };
