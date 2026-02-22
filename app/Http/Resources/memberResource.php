00f23065221154399e98736908b9242b1b8bbe8c
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-22 06:06:01 +0430

    rewource and request for member

---
+++ app/Http/Resources/memberResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class memberResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+             "name"=>$this->name,
+             "email"=>$this->email,
+             "whatsApp_number"=>$this->whatsApp_number,
+             "address"=>$this->address,
+             "membership_date"=>$this->membership_date,
+        ];
+    }
+}
