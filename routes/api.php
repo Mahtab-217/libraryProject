3a005c747eedbf26685da12064c7cfb6816b3cd1
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-02-22 06:00:57 +0430

    controller for member

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\controllers\AuthorController;
 use App\Http\controllers\BookController;
+use App\Http\Controllers\MemberControler;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -11,3 +13,4 @@ Route::get('/user', function (Request $request) {
  Route::apiResource("author", AuthorController::class);
 // Rout::apiResource("book",bookController::class);
 Route::apiResource("book", BookController::class);
+Route::apiResource("member",MemberControler::class);
